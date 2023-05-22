9053e18e30e8a71580e1f7fd09676e5bcb41bb4b
ahmadharthey <ahmoood@outlook.com>
2023-05-22 21:27:01 +0300

    hosted database online

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 const mysql=require("mysql2");
 
 module.exports=mysql.createConnection({
-    host:"localhost",
-    user: "root",
-    password:"superAdminFMH116@",
-    database:"FMHdatabase"
+    host:"bomra7g8delguwme15yw-mysql.services.clever-cloud.com",
+    port:"3306",
+    user: "u8nydfkcls0q659v",
+    password:"qbv1u99xlgvUnKmpXcDK",
+    database:"bomra7g8delguwme15yw"
 ,})
