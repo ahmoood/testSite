e6eb7896750d194e39b23a488a2796357b3f1a7c
ahmadharthey <ahmoood@outlook.com>
2023-05-30 22:17:35 +0300

    commit database to localhost

--- database.js
+++ database.js
@@ -1,9 +1,8 @@
 const mysql=require("mysql2");
 
 module.exports=mysql.createConnection({
-    host:"bomra7g8delguwme15yw-mysql.services.clever-cloud.com",
-    port:"3306",
-    user: "u8nydfkcls0q659v",
-    password:"qbv1u99xlgvUnKmpXcDK",
+    host:"localhost",
+    user: "root",
+    password:"database123$",
     database:"bomra7g8delguwme15yw"
 ,})
